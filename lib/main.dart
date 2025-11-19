3af147fdebfc82a903c438218252dd8e6384b174
hasanulmuttaqin <hasanul.muttaqin@ui.ac.id>
2025-11-19 15:25:43 +0700

    added workflows

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
-import 'package:football_news/screens/menu.dart';
 import 'package:football_news/screens/login.dart';
 
 void main() {
